c855a7b7356a147652ad4601e0ca3801aeccea80
Ramii Ahmed <ramy@ahmedramy.com>
2021-02-25 21:14:52 +0000

    sm8250-common: Add custom fingerprint HIDL HAL * Based on sm6250 common tree.

--- kona.mk
+++ kona.mk
@@ -201,8 +201,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+else ifeq ($(TARGET_NO_FOD),true)
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona
 endif
-
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
